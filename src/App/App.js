78ff8c5a72789de226ecd059a0df155f6b1d08ce
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-09-24 23:53:57 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
   render() {
     return (
-    <div>
+    <div className="App">
       <button className='btn btn-danger'>HELP ME</button>
     </div>
     );
